b476e3b6e119df2a9b475b4b092fc13de86f0028
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-11-05 01:43:45 +0300

    Add Sorting Features

--- Repositories/EFCore/Extensions/BookRepositoryExtension.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtension.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extensions
 {
@@ -25,6 +27,23 @@ namespace Repositories.EFCore.Extensions
             return books.Where(b => b.Title
                 .ToLower()
                 .Contains(lowerCaseTerm));
+        }
+        public static IQueryable<Book> Sort(this IQueryable<Book> books,
+            string orderByQueryString)
+        {
+            if(string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return books.OrderBy(b => b.Id);
+            }
+
+            var orderQuery = OrderQueryBuilder.CreateOrderByQuery<Book>(orderByQueryString);
+
+            if(orderQuery is null)
+            {
+                return books.OrderBy(b => b.Id);
+            }
+
+            return books.OrderBy(orderQuery);
         }
     }
 }
