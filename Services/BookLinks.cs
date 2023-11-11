f7fa3d8591368693dcac26f92f4d5a1c25760209
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-11-11 15:42:01 +0300

    iplementing part of BookLinks Class

---
+++ Services/BookLinks.cs
@@ -0,0 +1,38 @@
+﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BookLinks : IBookLinks
+    {
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IDataShaper<BookDto> _dataShaper;
+
+        public BookLinks(LinkGenerator linkGenerator, IDataShaper<BookDto> dataShaper)
+        {
+            _linkGenerator = linkGenerator;
+            _dataShaper = dataShaper;
+        }
+
+        public LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto, string fields, HttpContext HttpContext)
+        {
+            var shapedBook = ShapeData(booksDto, fields);
+        }
+
+        private List<Entity> ShapeData(IEnumerable<BookDto> booksDto, string fields)
+        {
+            return _dataShaper.ShapeData(booksDto, fields)
+                .Select(b => b.Entity)
+                .ToList();
+        }
+    }
+}
