f88b387166e952c7204edcb6d46d4634d805ed84
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-12-04 02:55:29 +0300

    Refresh Token fields , create , details

--- Presentetion/Controllers/AuthenticationController.cs
+++ Presentetion/Controllers/AuthenticationController.cs
@@ -40,16 +40,27 @@ namespace Presentetion.Controllers
         }
         [HttpPost("login")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
-        public async Task<IActionResult> Athenticate([FromBody]UserForAuthenticationDto user)
+        public async Task<IActionResult> Athenticate([FromBody] UserForAuthenticationDto user)
         {
-            if(!await _service.AuthenticationService.ValidateUser(user))
+            if (!await _service.AuthenticationService.ValidateUser(user))
             {
                 return Unauthorized();
             }
-            return Ok(new
-            {
-                Token = await _service.AuthenticationService.CreateToken()
-            });
+            var tokenDto = await _service
+                .AuthenticationService
+                .CreateToken(populateExp: true);
+
+            return Ok(tokenDto);
+        }
+
+        [HttpPost("refresh")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Refresh([FromBody] TokenDto tokenDto)
+        {
+            var tokenDtoToReturn = await _service
+                .AuthenticationService
+                .RefreshToken(tokenDto);
+            return Ok(tokenDtoToReturn);
         }
     }
 }
